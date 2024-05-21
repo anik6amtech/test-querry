a5aafc4c2e03942dd996d90e48cd4d04c5c50989
anik6amtech <anik.6amtech@gmail.com>
2024-05-21 14:39:53 +0600

    update

--- routes/rest_api/v1/api.php
+++ routes/rest_api/v1/api.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('/tenant/auth')->group(function () {
 
 });
+
+
+Route::get('/users', [UserController::class, 'index']);
