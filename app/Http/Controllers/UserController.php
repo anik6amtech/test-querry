23a39ee8782401a906276d920fe99fe3ea454374
anik6amtech <anik.6amtech@gmail.com>
2024-05-21 15:39:32 +0600

    up

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,12 +4,71 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class UserController extends Controller
 {
     public function index()
     {
         $users = User::all();
-        dd($users);
+    }
+
+
+    public function getUserByPhone($phone)
+    {
+
+        // Enable query logging
+        DB::enableQueryLog();
+        // Use Eloquent to join the users and userdetails tables
+        $user = User::join('user_details', 'users.id', '=', 'user_details.user_id')
+                    ->whereIn('user_details.phone', $phone)
+                    ->select('users.*', 'user_details.address', 'user_details.phone')
+                    ->get();
+
+       // Get the query log
+       $queries = DB::getQueryLog();
+
+       // Calculate the execution time of the query
+       $executionTime = $queries[0]['time'];
+
+       // Prepare the results
+       $result = [
+           'user' => $user,
+           'execution_time' => $executionTime . ' ms',
+           'query' => $queries[0]['query']
+       ];
+
+       // Return the result as JSON
+       dd($result);
+    }
+
+
+    public function searchUserByPhoneOrAddress($search)
+    {
+        // Enable query logging
+        DB::enableQueryLog();
+
+        // Execute the query to join the users and userdetails tables
+        $users = User::join('user_details', 'users.id', '=', 'user_details.user_id')
+                    ->where('user_details.phone', 'like', '%' . $search . '%')
+                    ->orWhere('user_details.address', 'like', '%' . $search . '%')
+                    ->select('users.*', 'user_details.address', 'user_details.phone')
+                    ->get();
+
+        // Get the query log
+        $queries = DB::getQueryLog();
+
+        // Calculate the execution time of the query
+        $executionTime = $queries[0]['time'];
+
+        // Prepare the results
+        $result = [
+            'users' => $users,
+            'execution_time' => $executionTime . ' ms',
+            'query' => $queries[0]['query']
+        ];
+
+        // Return the result as JSON
+        dd($result);
     }
 }
--- routes/rest_api/v1/api.php
+++ routes/rest_api/v1/api.php
@@ -10,3 +10,5 @@ Route::prefix('/tenant/auth')->group(function () {
 
 Route::get('/users', [UserController::class, 'index']);
 Route::get('/', [UserController::class, 'index']);
+Route::get('/users/phone/{phone}', [UserController::class, 'getUserByPhone']);
+Route::get('/users/search/{search}', [UserController::class, 'searchUserByPhoneOrAddress']);
